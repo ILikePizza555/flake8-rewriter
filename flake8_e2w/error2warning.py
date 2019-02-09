59a16e27737e69a7ff658e6c0143fa486875ac18
Isaac Avram <avrisaac555@gmail.com>
2019-02-08 17:22:46 -0800

    Moved add_options to correct place and fixed some errors

--- flake8_e2w/error2warning.py
+++ flake8_e2w/error2warning.py
@@ -1,4 +1,19 @@
 from flake8.formatting import default
+from flake8.style_guide import Violation
+
+
+def add_options(option_manager):
+        option_manager.add_option("--replace", action="append", dest="replacements", help="Given <code1>:<code2>, replaces all isntances of <code1> with <code2>.")
+
+
+def rewrite_violation(violation, new_code):
+    return Violation(
+        new_code,
+        violation.filename,
+        violation.line_number,
+        violation.column_number,
+        violation.text,
+        violation.physical_line)
 
 
 class RewriteFormatter(default.Default):
@@ -8,12 +23,12 @@ class RewriteFormatter(default.Default):
         super().__init__(options)
 
         self.replacements = dict()
-        for k, v in map(lambda s: s.split(":", 1), options.replacements):
-            self.replacements[k] = v
-    
-    def add_options(self, option_manager):
-        option_manager.add_option("--replace", action="append", dest="replacements", help="Given <code1>:<code2>, replaces all isntances of <code1> with <code2>.")
+        if hasattr(options, "replacements"):
+            for k, v in map(lambda s: s.split(":", 1), options.replacements):
+                self.replacements[k] = v
     
     def format(self, error):
-        error.code = self.replacements.get(error.code, error.code)
-        super().format(error)
+        if error.code in self.replacements:
+            error = rewrite_violation(error, self.replacements[error.code])
+
+        return super().format(error)
